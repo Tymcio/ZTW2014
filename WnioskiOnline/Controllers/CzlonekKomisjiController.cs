af32c6cbfa79e28db11e20005c4e19e628df1389
paulina <konieczna.paulina00@gmail.com>
2014-04-08 00:12:47 +0200

    czlonek komisji - widoki

---
+++ WnioskiOnline/Controllers/CzlonekKomisjiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WnioskiOnline.Controllers
+{
+    public class CzlonekKomisjiController : Controller
+    {
+        //
+        // GET: /CzlonekKomisji/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
