effbb5c52f949e030324296148b41e4139a0451a
Marcin <tymciom@gmail.com>
2014-04-07 22:00:59 +0200

    Menu, css

---
+++ WnioskiOnline/Controllers/RecenzentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WnioskiOnline.Controllers
+{
+    public class RecenzentController : Controller
+    {
+        //
+        // GET: /Recenzent/
+
+        public ActionResult Recenzje()
+        {
+            ViewBag.Message = "Recenzje";
+
+            return View();
+        }
+
+    }
+}
