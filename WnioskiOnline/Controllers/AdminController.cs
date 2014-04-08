183d1bbdfa066debf5242467bea46dd7186482bd
paulina <konieczna.paulina00@gmail.com>
2014-04-08 02:44:34 +0200

    Admin - konkursy, szczegoly konkursu, uzytkownicy

---
+++ WnioskiOnline/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WnioskiOnline.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Konkursy()
+        {
+            return View();
+        }
+
+        public ActionResult SzczegolyKonkursu()
+        {
+            return View();
+        }
+
+        public ActionResult Uzytkownicy()
+        {
+            return View();
+        }
+
+    }
+}
