d9619755a76e26c2eb639f153f17cd3b2fff58d4
Marcin <tymciom@gmail.com>
2014-04-08 12:03:29 +0200

    Kontroler komisji

---
+++ WnioskiOnline/Controllers/KomisjaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WnioskiOnline.Controllers
+{
+    public class KomisjaController : Controller
+    {
+        //
+        // GET: /Komisja/
+
+        public ActionResult Wnioski()
+        {
+            ViewBag.Message = "Wnioski";
+
+            return View();
+        }
+
+        public ActionResult SzczegolyWnioskow()
+        {
+            ViewBag.Message = "Wnioski - szczegóły";
+
+            return View();
+        }
+
+    }
+}
