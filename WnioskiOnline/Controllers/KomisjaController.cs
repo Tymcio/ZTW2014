98b8e8384feb4147ed384bdbec78a846d3a63f56
paulina <konieczna.paulina00@gmail.com>
2014-05-02 18:35:41 +0200

    sync2

---
+++ WnioskiOnline/Controllers/KomisjaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WnioskiOnline.Controllers
+{
+    public class KomisjaController : Controller
+    {
+        //
+        // GET: /CzlonekKomisji/
+
+
+        public ActionResult Wnioski()
+        {
+            ViewBag.Message = "Wnioski";
+
+            return View();
+        }
+
+        public ActionResult SzczegolyWnioskow()
+        {
+            ViewBag.Message = "Szczegóły wniosków";
+
+            return View();
+        }
+
+    }
+}
